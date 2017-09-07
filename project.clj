d9a81c53673aeff331f4ec09eada3bb78023c93d
Julie S. Mike <juliesmike@gmail.com>
2017-09-06 20:37:04 -0500

    project description in project file

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clojure-chatter2 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Web app to display messages"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
