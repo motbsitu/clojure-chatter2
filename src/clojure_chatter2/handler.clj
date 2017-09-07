454f46f6119a868a5ca399bb38b5c2b73f5db9be
Julie S. Mike <juliesmike@gmail.com>
2017-09-07 18:22:37 -0500

    start of chat messages

--- src/clojure_chatter2/handler.clj
+++ src/clojure_chatter2/handler.clj
@@ -4,16 +4,23 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [hiccup.page :as page]))
 
-(defn generate-message-view
-  "This generates the HTML for displaying messages"
-  []
-  (page/html5
-   [:head
-    [:title "chatter"]]
-   [:body
-    [:h1 "Our Chat App"]]))
+      (defn generate-message-view
+        "This generates the HTML for displaying messages"
+        [messages]
+        (page/html5
+         [:head
+          [:title "chatter"]]
+         [:body
+          [:h1 "Our Chat App"]
+          [:p (str messages)]]))
+
+
+(def chat-messages [{:name "blue" :message "hello, world"}
+                    {:name "red" :message "red is my favorite color"}
+                    {:name "green" :message "green makes it go faster"}])
+
 (defroutes app-routes
-  (GET "/" [] (generate-message-view))
+  (GET "/" [] (generate-message-view chat-messages))
   (route/not-found "Not Found"))
 
 (def app
